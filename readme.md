d611eee730f39f0c48dc248646aef859add913a1
Matthew Kirkley <matt.kirkley@gmail.com>
2013-11-22 21:40:19 -0500

    Sample groovy gradle project.

---
+++ readme.md
@@ -0,0 +1,6 @@
+Sample Groovy project built with Gradle.
+
+This shows how to compile as well as run a groovy script from Gradle.
+
+To run the sample script type
+'gradle run'
---
+++ src/main/groovy/edu/kirkley/sample/HelloGroovy.groovy
@@ -0,0 +1,17 @@
+package edu.kirkley.sample
+
+class HelloGroovy {
+
+    static void main(args) {
+        new HelloGroovy().run()
+    }
+    
+    HelloGroovy() {
+
+    }
+
+    void run() {
+        println "hello world!"
+    }
+
+}
